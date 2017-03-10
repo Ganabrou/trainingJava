b6c6fb89468d81d34b42af2d84726a21dd649dca
Ganabrou <huongphan2025@gmail.com>
2017-03-10 17:46:31 +0700

    BTT5

---
+++ GravityCalculator.java
@@ -0,0 +1,43 @@
+//Baitaptuan
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ha.abc;
+
+/**
+ *
+ * @author HOANGANH
+ */
+// Part1
+public class GravityCalculator {
+    public static void main(String[] arguments){
+double gravity =-9.81; // Earth's gravity in m/s^2
+double initialVelocity = 0.0;
+double fallingTime = 10.0;
+double initialPosition = 0.0;
+double finalPosition = 0.0;
+System.out.println("The object's position after " + fallingTime +
+" seconds is " + finalPosition + " m.");
+}
+}
+ /* Chương trình đầu ra không thay đổi cụm từ đặt trong ""   
+    E không hiểu câu hỏi lắm nên trả lời thế
+    Ý câu hỏi là gì ạ?
+ */
+/* Part2
+public static void main(String[] arguments)
+{ int t=10;
+  double a= -9.81;
+  double v=0.0, x=0.0;
+System.out.println("The object's póition after " + t + "seconds is" + 
+         (0.5+a*t*t+v*t+x) + "m.");
+        
+                     
+}
+    
+
+
+*/
+  
